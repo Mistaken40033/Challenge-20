ca2db117228443b401b456caa8011132ca3a7938
Erica Balderas <ericab40033@hotmail.com>
2024-07-20 15:28:27 -0600

    adding Raccoon image

--- my-portfolio/src/sections/Portfolio.jsx
+++ my-portfolio/src/sections/Portfolio.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import Project from '../components/Project';
 import bluePainting from './bluepainting_by_me.png'; // Import the image
+import Raccoon from '../sections/'
 
 const projects = [
-  { title: 'Project 1', image: bluePainting, appLink: 'http://app1.com', repoLink: 'https://github.com/Mistaken40033' },
+  { title: 'Project 1', image: bluePainting, appLink: '', repoLink: 'git@github.com:Mistaken40033/Challenge-19.git' },
   // Add more projects
+  { title: 'Project 1', image: Raccoon, appLink: '', repoLink: 'git@github.com:Mistaken40033/blog-beats.git' },
 ];
 
 const Portfolio = () => (
