678357bb2f4882b5447e953b2a2fa350ed349790
Erica Balderas <ericab40033@hotmail.com>
2024-07-21 18:45:06 -0600

    adding different font size

--- my-portfolio/src/sections/Resume.jsx
+++ my-portfolio/src/sections/Resume.jsx
@@ -5,8 +5,10 @@ const Resume = () => (
       <h2>Resume</h2>
       <a href="path-to-resume.pdf" download>Download Resume</a>
       <ul>
-        <li>Proficiency 1</li>
-        <li>Proficiency 2</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>JAVASCRIPT</li>
       </ul>
       </div>
     </section>
